57d1ed43c8006075aad3b9a807be1c5a04379e7c
Sanderson Coelho <sanderson.coelho@ctis.com.br>
2019-05-16 21:41:21 -0400

    Mongo property file

--- src/main/java/com/wirecard/repository/MongoConfig.java
+++ src/main/java/com/wirecard/repository/MongoConfig.java
@@ -2,18 +2,26 @@ package com.wirecard.repository;
 
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
 import com.mongodb.MongoClient;
 
 @Configuration
+@PropertySource("classpath:application.properties")
 public class MongoConfig {
 
-    private static final String MONGO_DB_URL = "localhost";
-    private static final Integer MONGO_DB_PORT = 27017;
-    private static final String MONGO_DB_NAME = "wirecard";
+	@Value("${mongo.db.url}")
+    private String MONGO_DB_URL;
+    
+	@Value("${mongo.db.port}")
+	private Integer MONGO_DB_PORT;
+	
+	@Value("${mongo.db.name}")
+	private String MONGO_DB_NAME;
     
     @Bean
     public MongoTemplate mongoTemplate() throws IOException {
